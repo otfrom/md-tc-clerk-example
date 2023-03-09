3659b98c0347bc172ec4b2368122e792b0958e96
Bruce Durling <bruce@mastodonc.com>
2023-03-09 17:28:08 +0000

    Better tmd/tc table handling

--- notebooks/tc_md_example.clj
+++ notebooks/tc_md_example.clj
@@ -8,10 +8,10 @@
 
 ;; You can create markdown tables by concating some strings together like
 ;; so.
-;; 
+;;
 ;; The syntax for markdown tables is [here](https://www.markdownguide.org/extended-syntax/#tables).
-;; 
-(clerk/md 
+;;
+(clerk/md
  (str
   "| Foo | Bar | Baz |\n"
   "| :--- | :---: | ---: |\n"
@@ -29,20 +29,20 @@
 (defn md-table
   [{:keys [columns alignment-spec data]}]
   (clerk/md
-   (apply 
+   (apply
     str
     (into [(md-row columns)
            (md-row alignment-spec)]
           (comp
-           (map 
+           (map
             (fn [row]
-              ((apply juxt 
+              ((apply juxt
                       (map (fn [column] (fn [m] (m column))) columns))
                row)))
            (map md-row))
           (tc/rows data :as-maps)))))
 
-(md-table 
+(md-table
  {:columns ["Foo" "Bar" "Baz"]
   :alignment-spec [":---" ":---:" "---:"]
   :data (tc/dataset {"Foo" ["foo0" "foo1" "foo2"]
@@ -50,10 +50,20 @@
                      "Baz" [12.0 1.00 1.01]})})
 
 
-;; You can also use `p/dataset->str` with `{:print-line-policy :markdown}`
+;; You can also use `p/dataset->str` with `{:print-line-policy:markdown}`
+;;
+;; You need to strip out the table name as well if you don’t want that.
+;;
+;; You can do that with this lovely multiline java regular expression:
+;;
+;; `#"^(?s).+\n\n(.+)"`
+;;
+;; The `(?s)` at the front makes it multiline.
 
-(clerk/md 
- (-> (tc/dataset {"Foo" ["foo0" "foo1" "foo2"]
-                  "Bar" ["bar0" "bar1" "bar2"]
-                  "Baz" [12.0 1.00 1.01]})
-     (p/dataset->str {:print-line-policy :markdown})))
+(clerk/md
+ (as-> (tc/dataset {"Foo" ["foo0" "foo1" "foo2"]
+                    "Bar" ["bar0" "bar1" "bar2"]
+                    "Baz" [12.0 1.00 1.01]}) $
+   (p/dataset->str $ {:print-line-policy :markdown})
+   (re-find #"^(?s).+\n\n(.+)" $) ;; strip out the dataset name
+   (last $)))
